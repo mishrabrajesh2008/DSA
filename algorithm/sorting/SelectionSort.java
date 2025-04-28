f6bdb8ec9a387606310e1cc396a9fc412d083cbb
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-04-28 12:47:11 +0530

    selection short commit and file formatting

--- algorithm/sorting/SelectionSort.java
+++ algorithm/sorting/SelectionSort.java
@@ -3,20 +3,27 @@ package sorting;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BubbleSort {
+public class SelectionSort {
 
-	public static void bubbleSort(int[] array) {
-		for (int i = array.length - 1; i > 0; i--) {
-			for (int j = 0; j < i; j++) {
-				if (array[j] > array[j + 1]) {
-					int temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
+	
+	public static void selectionSort(int[] array) {
+		for(int i=0;i<array.length;i++) {
+			int minIndex = i;
+			for(int j=i+1;j<array.length;j++) {
+				if(array[j]<array[minIndex]) {
+					minIndex = j;
 				}
+			}
+			if(i!=minIndex) {
+				int temp = array[i];
+				array[i] = array[minIndex];
+				array[minIndex] = temp;
 			}
 		}
 	}
-
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.print("Enter array length: ");
 		Scanner sc = new Scanner(System.in);
@@ -27,7 +34,7 @@ public class BubbleSort {
 			array[i] = sc.nextInt();
 		}
 
-		bubbleSort(array);
+		selectionSort(array);
 		System.out.println(Arrays.toString(array));
 		sc.close();
 	}
