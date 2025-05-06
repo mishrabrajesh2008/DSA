c5035b1ccd1778934b55b2677852b91830aaa7d2
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-06 16:08:12 +0530

    PartitionList

---
+++ datastructure/linkedlist_leetcode/PartitionList.java
@@ -0,0 +1,136 @@
+package linkedlist_leetcode;
+
+public class PartitionList {
+	
+	private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public PartitionList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    public void partitionList(int x){
+        if (head == null) return;
+        Node temp = head;
+    	Node D1=new Node(x);
+    	Node prev1=D1;
+    	Node D2=new Node(x);
+    	Node prev2 = D2;
+        while(temp!=null){
+            if(temp.value>=x){
+                prev2.next = temp;            
+                prev2=temp;
+            }else{
+                prev1.next = temp;
+                prev1 = temp;
+            }
+            temp = temp.next;
+            prev2.next=null;
+            prev1.next=D2.next;
+            head = D1.next;
+        }
+    }
+
+	
+	public static void main(String[] args) {
+		PartitionList ll = new PartitionList(3);
+        ll.append(5);
+        ll.append(8);
+        ll.append(10);
+        ll.append(2);
+        ll.append(1);
+
+        // Print the list before partitioning
+        System.out.println("LL before partitionList:");
+        ll.printList(); // Output: 3 5 8 10 2 1
+
+        // Call the partitionList method with x = 5
+        ll.partitionList(5);
+
+        // Print the list after partitioning
+        System.out.println("LL after partitionList:");
+        ll.printList(); // Output: 3 2 1 5 8 10
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            LL before partition_list:
+            3
+            5
+            8
+            10
+            2
+            1
+            LL after partition_list:
+            3
+            2
+            1
+            5
+            8
+            10
+
+        */
+	}
+
+}
