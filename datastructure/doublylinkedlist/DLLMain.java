ec3efe196dd751252455a2ac901c4b5ec03bbac2
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-08 23:08:46 +0530

    Doubly linkedlist constructor

--- datastructure/doublylinkedlist/DLLMain.java
+++ datastructure/doublylinkedlist/DLLMain.java
@@ -3,7 +3,8 @@ package doublylinkedlist;
 public class DLLMain {
 	
 	public static void main(String[] args) {
-		
+		DoublyLinkedList dll = new DoublyLinkedList(7);
+		dll.printAll();
 	}
 
 }
--- datastructure/doublylinkedlist/DoublyLinkedList.java
+++ datastructure/doublylinkedlist/DoublyLinkedList.java
@@ -1,5 +1,61 @@
 package doublylinkedlist;
 
 public class DoublyLinkedList {
+	private Node head;
+	private Node tail;
+	private int length;
+	
+	class Node{
+		Node next;
+		Node prev;
+		int value;
+		
+		public Node(int value){
+			this.value = value;
+		}
+		
+	}
+		public Node getHead() {
+			return head;
+		}
+		
+		public Node getTail() {
+			return tail;
+		}
+		
+		public int getLength() {
+			return length;
+		}
+		
+		public void printList() {
+	        Node temp = head;
+	        while (temp != null) {
+	            System.out.println(temp.value);
+	            temp = temp.next;
+	        }
+	    }
+		
+		public void printAll() {
+	        if (length == 0) {
+	            System.out.println("Head: null");
+	            System.out.println("Tail: null");
+	        } else {
+	            System.out.println("Head: " + head.value);
+	            System.out.println("Tail: " + tail.value);
+	        }
+	        System.out.println("Length:" + length);
+	        System.out.println("\nDoubly Linked List:");
+	        if (length == 0) {
+	            System.out.println("empty");
+	        } else {
+	            printList();
+	        }
+	    }
 
+	public DoublyLinkedList(int value){
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
 }
