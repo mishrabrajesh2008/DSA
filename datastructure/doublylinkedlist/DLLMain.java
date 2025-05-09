33623bd67837830dfdb9044774839839d7121347
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-09 07:36:44 +0530

    Doubly Linked list append

--- datastructure/doublylinkedlist/DLLMain.java
+++ datastructure/doublylinkedlist/DLLMain.java
@@ -4,6 +4,9 @@ public class DLLMain {
 	
 	public static void main(String[] args) {
 		DoublyLinkedList dll = new DoublyLinkedList(7);
+		dll.append(9);
+		dll.append(11);
+		dll.append(19);
 		dll.printAll();
 	}
 
