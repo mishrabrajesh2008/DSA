8ce5c4a0ed1864bc7804ca19f81fbdee07ed0702
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-22 07:24:30 +0530

    Hashtable find duplicates

--- datastructure/hashtables_leetcode/FindDuplicates.java
+++ datastructure/hashtables_leetcode/FindDuplicates.java
@@ -1,9 +1,32 @@
 package hashtables_leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class FindDuplicates {
+	
+	public static List<Integer> findDuplicates(int[] nums){
+		List<Integer> myList = new ArrayList<Integer>();
+		HashMap<Integer, Boolean> myMap = new HashMap<>();
+		
+		 for(int i: nums) {
+			 if(myMap.containsKey(i) && !myList.contains(i)) {
+				 myList.add(i);
+			 }else {
+				 myMap.put(i, true);
+			 }
+		 }
+		return myList;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {1, 2, 3, 2, 1, 4, 5, 4};
+		int[] nums1 = {1,1,1,1};
+		List<Integer> duplicates = findDuplicates(nums);
+        List<Integer> duplicates1 = findDuplicates(nums1);
+        System.out.println(duplicates); 
+        System.out.println(duplicates1); 
 
 	}
 
