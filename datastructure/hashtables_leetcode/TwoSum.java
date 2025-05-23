eb4619caf8248146863e2e4e230d5adf719ce97a
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-23 15:57:31 +0530

    Hashtable Two Sums

---
+++ datastructure/hashtables_leetcode/TwoSum.java
@@ -0,0 +1,39 @@
+package hashtables_leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+	
+	public static int[] twoSum(int[] nums, int target){
+		Map<Integer, Integer> twoSums = new HashMap<>();
+		
+		for(int i = 0; i<nums.length;i++) {
+			int num = nums[i];
+			int complement = target-num;
+			if(twoSums.containsKey(complement)) {
+				return new int[]{twoSums.get(complement), i};
+			}
+			twoSums.put(num, i);
+		}
+		return new int[]{};
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(twoSum(new int[] { 2, 7, 11, 15 }, 9)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 3, 2, 4 }, 6)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 3, 3 }, 6)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 2, 3, 4, 5 }, 10)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 2, 3, 4, 5 }, 7)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 1, 2, 3, 4, 5 }, 3)));
+		System.out.println(Arrays.toString(twoSum(new int[] {}, 0)));
+
+		/*
+		 * EXPECTED OUTPUT: ---------------- [0, 1] [1, 2] [0, 1] [] [2, 3] [0, 1] []
+		 * 
+		 */
+
+	}
+
+}
