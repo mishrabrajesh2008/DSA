884fb91d53e9ee4169bd8013791be50add02fecc
brajesh.mishra <brajesh.mishra@IN-55SHK24.calsofthq.com>
2025-05-24 19:01:17 +0530

    Hashtable remove duplicates using set

---
+++ datastructure/hashtables_leetcode/RemoveDuplicateSet.java
@@ -0,0 +1,36 @@
+package hashtables_leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveDuplicateSet {
+	
+	public static List<Integer> removeDuplicates(List<Integer> myList){
+		
+		Set<Integer> mySet = new HashSet<>();
+				
+		for(int i: myList) {
+			mySet.add(i);
+		}		
+		return new ArrayList<Integer>(mySet);
+		
+	}
+
+	public static void main(String[] args) {
+		List<Integer> myList = List.of(1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5);
+        List<Integer> newList = removeDuplicates(myList);
+        System.out.println(newList);
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+            (Order may be different as sets are unordered)
+        */
+
+	}
+
+}
